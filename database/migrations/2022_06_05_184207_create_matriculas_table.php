e08c29502762beaee0bc73927ee713b4aaa6b966
WelderOliveira <weldersilva326@gmail.com>
2022-06-05 16:15:56 -0300

    Create Eloquent DB and Relations

---
+++ database/migrations/2022_06_05_184207_create_matriculas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatriculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matriculas', function (Blueprint $table) {
+            $table->foreignId('aluno_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('curso_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matriculas');
+    }
+}
